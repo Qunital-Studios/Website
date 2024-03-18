cb2d9b4355b3f790cb3d28ac1d771a701d55878c
IMikeRS <Miki@qunitalstudioss.onmicrosoft.com>
2024-03-18 17:40:41 +0100

    General update

--- scripts/index.js
+++ scripts/index.js
@@ -18,29 +18,24 @@ var headerMenuButton = document.querySelector(".headerMenuButton");
 var headerMenu = document.querySelector(".headerMenu");
 var headerMenuIsClicked = false;
 headerMenuButton.addEventListener("click", () => {
-        headerMenuIsClicked = !headerMenuIsClicked;
-        if(headerMenuIsClicked){
-            Array.from(headerMenuButton.children).forEach(child => {
+    headerMenuIsClicked = !headerMenuIsClicked;
+    if(headerMenuIsClicked){
+        Array.from(headerMenuButton.children).forEach(child => {
             child.classList.add("clicked");
             headerMenuButton.classList.add("clicked");
             headerMenu.classList.add("show");
             Array.from(headerMenu.children).forEach(child => {
                 child.classList.add("show")
             })
-            setTimeout( () => {
-                document.querySelector(".bannerVideo").pause();
-            }, 400)
-            })
-        }else{
-            Array.from(headerMenuButton.children).forEach((child) => {
-                child.classList.remove("clicked");
-                headerMenuButton.classList.remove("clicked");
-            })
-            headerMenu.classList.remove("show");
-            Array.from(headerMenu.children).forEach(child => {
-                child.classList.remove("show")
-            })
-            document.querySelector(".bannerVideo").play();
-        }
-    
+        })
+    }else{
+        Array.from(headerMenuButton.children).forEach((child) => {
+            child.classList.remove("clicked");
+            headerMenuButton.classList.remove("clicked");
+        })
+        headerMenu.classList.remove("show");
+        Array.from(headerMenu.children).forEach(child => {
+            child.classList.remove("show")
+        })
+    }
 })
