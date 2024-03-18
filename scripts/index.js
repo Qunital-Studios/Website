fd67f29ba07f5b79cef878de8498fe4ea3433bd6
IMikeRS <Miki@qunitalstudioss.onmicrosoft.com>
2024-03-18 16:14:16 +0100

    General update

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,34 @@ const  observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((element) => observer.observe(element));
+
+var headerMenuButton = document.querySelector(".headerMenuButton");
+var headerMenu = document.querySelector(".headerMenu");
+var headerMenuIsClicked = false;
+headerMenuButton.addEventListener("click", () => {
+        headerMenuIsClicked = !headerMenuIsClicked;
+        if(headerMenuIsClicked){
+            Array.from(headerMenuButton.children).forEach(child => {
+            child.classList.add("clicked");
+            headerMenuButton.classList.add("clicked");
+            headerMenu.classList.add("show");
+            Array.from(headerMenu.children).forEach(child => {
+                child.classList.add("show")
+            })
+            setTimeout( () => {
+                document.querySelector(".bannerVideo").pause();
+            }, 400)
+            })
+        }else{
+            Array.from(headerMenuButton.children).forEach((child) => {
+                child.classList.remove("clicked");
+                headerMenuButton.classList.remove("clicked");
+            })
+            headerMenu.classList.remove("show");
+            Array.from(headerMenu.children).forEach(child => {
+                child.classList.remove("show")
+            })
+            document.querySelector(".bannerVideo").play();
+        }
+    
+})
